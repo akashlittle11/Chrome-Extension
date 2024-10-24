6ff52c4fb4ce4fecd39e34a1e12bbfdd5ddc01bc
akashlittle11 <115180106+akashlittle11@users.noreply.github.com>
2024-10-24 13:08:12 +0530

    Add files via upload

---
+++ chrome-extension-main/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MessageInput from './components/MessageInput';
+
+const App: React.FC = () => {
+  return (
+    <div className="p-4">
+      <MessageInput />
+    </div>
+  );
+};
+
+export default App;
